0d3dba0dc5f076fe28bf0da80862dbbb64762b5d
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-05-01 01:44:56 -0300

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
@@ -8,4 +10,18 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.join(__dirname, 'src') }],
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/utils/tests/setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
+  },
 })
